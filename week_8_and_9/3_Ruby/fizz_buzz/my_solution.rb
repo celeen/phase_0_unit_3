c4f890d82cbf2c1dc0963f52719b9441fafbeab5
celeen <celeenrusk@gmail.com>
2014-05-18 16:03:22 -0400

    Release 3, FIZZBUZZ

--- week_8_and_9/3_Ruby/fizz_buzz/my_solution.rb
+++ week_8_and_9/3_Ruby/fizz_buzz/my_solution.rb
@@ -1,21 +1,24 @@
-# U3.W8-9: 
-
-
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with: Zach Pflederer
 
 # 2. Pseudocode
-
+#
 
 
 # 3. Initial Solution
-
 def super_fizzbuzz(array)
-
-
+  array.map! do |n|
+    if n%3 == 0 && n%5 == 0
+      n = "FizzBuzz"
+    elsif n%3 == 0
+      n = "Fizz"
+    elsif n%5 == 0
+      n = "Buzz"
+    else
+      n
+    end
+  end
+  p array
 end
-
-
-
 # 4. Refactored Solution
 
 
@@ -25,9 +28,17 @@ end
 
 # 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
 
+def assert
+    raise 'nope' unless yield
+    puts yield
+end
+
+assert { super_fizzbuzz([3]) == ['Fizz'] }
+assert { super_fizzbuzz([3, 3, 3]) == ['Fizz', 'Fizz', 'Fizz'] }
+assert { super_fizzbuzz([5]) == ['Buzz'] }
+assert { super_fizzbuzz([15]) == ['FizzBuzz'] }
+assert { super_fizzbuzz([1, 2, 3]) == [1, 2, 'Fizz'] }
+assert { super_fizzbuzz([15, 5, 3, 1]) == ['FizzBuzz', 'Buzz', 'Fizz', 1] }
 
 
-
-
-
-# 5. Reflection 
+# 5. Reflection
