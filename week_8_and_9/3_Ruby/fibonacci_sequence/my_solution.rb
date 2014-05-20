7cd2ab1443c884bf3d01f4619bfb3c9591320266
celeen <celeenrusk@gmail.com>
2014-05-20 18:10:14 -0500

    Fibonacci Initial Solution

--- week_8_and_9/3_Ruby/fibonacci_sequence/my_solution.rb
+++ week_8_and_9/3_Ruby/fibonacci_sequence/my_solution.rb
@@ -1,24 +1,45 @@
 # U3.W8-9: 
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
 #define a method is_fibonacci? which takes one argument, an integer, and checks to see if it is in the fibonacci sequence.
+#__________________
 #If the number input is x,
 #if 5(x^2) + or - 4 is a square (or both) then x is in the fibonacci sequence
+#__________________
+#Method takes one argument, a number
+#we start with an array containing two members: 0, and 1
+#while the last member of the array is less than or equal to the input number,
+#add the last two member of the array together, and push that sum to the end of the array
+#Once outside the loop, check to see if the final array includes the input number 
 
 
 
 # 3. Initial Solution
-require 'bigdecimal'
 
 def is_fibonacci?(num)
-	if Math.sqrt((num**2)*5-4) % 1 == 0 || Math.sqrt((num**2)*5+4) % 1 == 0
-		return true
-	else return false
+	fibonacci = [0,1]
+	while fibonacci.last <= num 
+		fibonacci << fibonacci.last(2).reduce(:+)	
 	end
+
+	fibonacci.include?(num)
 end
+
+
+
+
+
+# require 'bigdecimal'
+
+# def is_fibonacci?(num)
+# 	if Math.sqrt((num**2)*5-4) % 1 == 0 || Math.sqrt((num**2)*5+4) % 1 == 0
+# 		return true
+# 	else return false
+# 	end
+# end
 
 
 
@@ -47,8 +68,7 @@ assert { is_fibonacci?(8670007398507948658051921) == true }
 
 p "3. returns false when number is not in fibonacci sequence"
 assert { is_fibonacci?(random_fibonacci + 100) == false }
-p "___________"
-p is_fibonacci?(927372692193078999171)
+
 p "4. returns false on large non-fibonacci numbers"
 assert { is_fibonacci?(927372692193078999151) == false }
 
