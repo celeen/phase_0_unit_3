a07c48b82fe39fd2c45cacae3e990219188c5f24
celeen <celeenrusk@gmail.com>
2014-05-20 10:14:52 -0500

    Fibonacci Sequence, some more work

--- week_8_and_9/3_Ruby/fibonacci_sequence/my_solution.rb
+++ week_8_and_9/3_Ruby/fibonacci_sequence/my_solution.rb
@@ -4,13 +4,20 @@
 # I worked on this challenge [by myself, with: ].
 
 # 2. Pseudocode
+#define a method is_fibonacci? which takes one argument, an integer, and checks to see if it is in the fibonacci sequence.
+#If the number input is x,
+#if 5(x^2) + or - 4 is a square (or both) then x is in the fibonacci sequence
 
 
 
 # 3. Initial Solution
+require 'bigdecimal'
 
 def is_fibonacci?(num)
-
+	if Math.sqrt((num**2)*5-4) % 1 == 0 || Math.sqrt((num**2)*5+4) % 1 == 0
+		return true
+	else return false
+	end
 end
 
 
@@ -40,9 +47,11 @@ assert { is_fibonacci?(8670007398507948658051921) == true }
 
 p "3. returns false when number is not in fibonacci sequence"
 assert { is_fibonacci?(random_fibonacci + 100) == false }
-
+p "___________"
+p is_fibonacci?(927372692193078999171)
 p "4. returns false on large non-fibonacci numbers"
-assert { is_fibonacci?(927372692193078999171) == false }
+assert { is_fibonacci?(927372692193078999151) == false }
+
 
 
 
