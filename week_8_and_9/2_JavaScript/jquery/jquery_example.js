7fc87988128c15c8a3f495b1267042cfa542a61a
celeen <celeenrusk@gmail.com>
2014-05-24 22:48:54 -0500

    Release 3 JQuery

--- week_8_and_9/2_JavaScript/jquery/jquery_example.js
+++ week_8_and_9/2_JavaScript/jquery/jquery_example.js
@@ -4,14 +4,26 @@ $(document).ready(function(){
   //Link this script and the jQuery library to the jquery_example.html file and analyze what this code does. 
  
 $('body').css({'background-color': 'pink'})
+
  
 //RELEASE 1:
-  //Add code here to select elements of the DOM 
+  
+
  
  
 //RELEASE 2: 
-  // Add code here to modify the css and html of DOM elements
- 
+$('h1').css({'color': 'blue', 'border': '5px solid red', 'visibility': 'inherit'})
+$('div.mascot').html('<h1>Coyotes 2014</h1><br><img src="dbc_logo.jpg">');
+$('img')
+	.mouseover(function(e) {
+	e.preventDefault()
+	$(this).attr('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMNI6hx7H9u_zVcGF-l8ekBJuJZNgDYr3HyPQttsJkWNNKlVlq');
+	})
+	.mouseout(function(e) {
+		$(this).attr('src', 'dbc_logo.jpg');
+	})
+
+
  
 //RELEASE 3: Event Listener
   // Add the code for the event listener here 
