c1429c3e5552d861e979593042b13de4880d5f55
celeen <celeenrusk@gmail.com>
2014-05-25 23:27:36 -0500

    reflection

--- week_8_and_9/3_Ruby/fizz_buzz/my_solution.rb
+++ week_8_and_9/3_Ruby/fizz_buzz/my_solution.rb
@@ -1,7 +1,13 @@
 # I worked on this challenge with: Zach Pflederer
 
 # 2. Pseudocode
-#
+#define a method named super_fizzbuzz
+#which takes an array as an argument
+#and which iterates through the array, checking each value for divisibility by 3 and 5
+#through a case statement, the first condition checks if value is divisible by both 3 and 5
+#second condition check divisibility by 3
+#third checks divisibility by 5
+#else return value
 
 
 # 3. Initial Solution
@@ -57,3 +63,5 @@ assert { super_fizzbuzz([15, 5, 3, 1]) == ['FizzBuzz', 'Buzz', 'Fizz', 1] }
 
 
 # 5. Reflection
+
+#I had a lot of fun working on this exercise with Zach. I keep feeling like there must be a way to combine the 3 and 5 tests, but with an array, I'm just not sure any method of doing so would be this succinct. Whe n the challenge is to print them, it would be relatively easy to use only two argument (3 and 5) rather than test for modulus 15 as well.
